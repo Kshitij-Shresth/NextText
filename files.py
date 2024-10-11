36c41e04115a9624fadc552e0fc225319d7b7945
Kshitij Shresth <kshitijshresth@icloud.com>
2024-10-11 21:31:19 +0100

    Update files.py

--- files.py
+++ files.py
@@ -2,3 +2,5 @@ text_df = pd.read_csv("data.csv")
 text = list(text_df['sentence'].values)
 joined_text = "".join(text)
 
+tokenizer = RegexpTokenizer(r"\w+")
+tokens = tokenizer.tokenize(partial_text.lower())
